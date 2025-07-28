2413e373270f01fa034201d7c43ec5f6eddb99b1
taholme <theodor@fjellstua.net>
2025-07-28 11:40:57 +0000

    changed to fish shell

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,19 @@
+if status is-login
+	fish_add_path ~/.local/bin ~/.bun/bin ~/.duckdb/cli/latest ~/.local/share/bob/nvim-bin
+	
+	if type -q keychain
+		keychain --quiet --eval id_ed25519 | source
+	end
+	
+	set TTY1 (tty)
+	[ "$TTY1" = /dev/tty1 ] && exec sway &>~/.Wsession.errors
+end
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    if set -q SSH_CONNECTION
+	    set -Ux EDITOR vim
+    else
+	    set -Ux EDITOR nvim
+    end
+end
